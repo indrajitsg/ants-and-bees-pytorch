97c82dc5d6d4902532a4cbf87eff2fedd3bccb0f
Indrajit Sen Gupta <indrajitsg@gmail.com>
2019-08-06 13:15:30 +0530

    Added str2bool for parser

--- train.py
+++ train.py
@@ -12,12 +12,20 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 import torch
 
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 parser = argparse.ArgumentParser(description='Test your model')
-parser.add_argument('--resume', default=False, help='Resume training from checkpoint')
-parser.add_argument('--reset_patience', default=False, help='Reset patience counter to 0')
-parser.add_argument('--use_cuda', default=False, help='Use CUDA for training')
-parser.add_argument('--output_model', help='Name of the output model')
+parser.add_argument('--resume', type=str2bool, default=False, nargs='?', help='Resume training from checkpoint')
+parser.add_argument('--reset_patience', type=str2bool, default=False, nargs='?', help='Reset patience counter to 0')
+parser.add_argument('--use_cuda', type=str2bool, default=False, nargs='?', help='Use CUDA for training')
 
 
 def main():
