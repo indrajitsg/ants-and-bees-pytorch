1a52424ca9f1f473bff168f15f993d9e7eb2ac09
Indrajit Sen Gupta <indrajitsg@gmail.com>
2019-08-06 16:24:28 +0530

    Updated repo for Alexnet training

--- ensemble.py
+++ ensemble.py
@@ -2,13 +2,14 @@ import torch.nn as nn
 from model.resnet import resnet18
 from model.inception import inception_v3
 from model.vgg import vgg16
+from model.alexnet import alexnet
 from data_loader.data_loader import get_data_loader
 from model.utils import LayerActivations
 from torch.utils.data import DataLoader
 
 model1 = vgg16(pretrained=False, num_classes=2)
 model2 = resnet18(pretrained=False, num_classes=2)
-model3 = inception_v3(pretrained=False, num_classes=2)
+model3 = alexnet(pretrained=False, num_classes=2)
 
 model1 = nn.Sequential(*list(model1.children())[:-2])
 
@@ -23,11 +24,10 @@ for p in model2.parameters():
 
 # print(model2)
 
-model3.aux_logits = False
 for p in model3.parameters():
     p.requires_grad = False
 
-# print(model3)
+print(model3)
 
 # Create data loader
 data_loader = get_data_loader()
@@ -65,17 +65,17 @@ for d, la in data_loader['val']:
     val_resnet_features.extend(out.data())
 
 # Extract features for Inception
-trn_inception_features = LayerActivations(model3.Mixed_7c)
+trn_alex_features = []
 for d, la in data_loader['train']:
-    _ = model3(d)
+    out = model3(d)
+    out = out.view(out.size(0), -1)
+    trn_alex_features.extend(out.data())
 
-trn_inception_features.remove()
-
-val_inception_features = LayerActivations(model3.Mixed_7c)
+val_alex_features = []
 for d, la in data_loader['val']:
-    _ = model3(d)
-
-val_inception_features.remove()
+    out = model3(d)
+    out = out.view(out.size(0), -1)
+    val_alex_features.extend(out.data())
 
 
 class FeaturesDataset(Dataset):
@@ -92,31 +92,10 @@ class FeaturesDataset(Dataset):
         return len(self.labellst)
 
 
-trn_feat_dset = FeaturesDataset(trn_vgg_features, trn_resnet_features, trn_inception_features, trn_labels)
-val_feat_dset = FeaturesDataset(val_vgg_features, val_resnet_features, val_inception_features, val_labels)
+trn_feat_dset = FeaturesDataset(trn_vgg_features, trn_resnet_features, trn_alex_features, trn_labels)
+val_feat_dset = FeaturesDataset(val_vgg_features, val_resnet_features, val_alex_features, val_labels)
 
 trn_feat_loader = DataLoader(trn_feat_dset,batch_size=64,shuffle=True)
 val_feat_loader = DataLoader(val_feat_dset,batch_size=64)
 
-
-class EnsembleModel(nn.Module):
-    def __init__(self, out_size, training=True):
-        super().__init__()
-        self.fc1 = nn.Linear(8192,512)
-        self.fc2 = nn.Linear(131072,512)
-        self.fc3 = nn.Linear(82944,512)
-        self.fc4 = nn.Linear(512,out_size)
-
-    def forward(self, inp1, inp2, inp3):
-        out1 = self.fc1(F.dropout(inp1, training=self.training))
-        out2 = self.fc2(F.dropout(inp2, training=self.training))
-        out3 = self.fc3(F.dropout(inp3, training=self.training))
-        out = out1 + out2 + out3
-        out = self.fc4(F.dropout(out, training=self.training))
-        return out
-
-
-em = EnsembleModel(out_size=2)
-
-if is_cuda:
-    em = em.cuda()
+print(trn_feat_loader)
